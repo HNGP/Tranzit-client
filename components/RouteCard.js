02106fb512c4d1b46d20d0e568e44a7d1ac5c19f
Ayush Neekhar <ayush@pop-os.localdomain>
2022-01-13 14:01:50 +0530

    Fixed mobile responsiveness

--- components/RouteCard.js
+++ components/RouteCard.js
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 import { Steps, Tag } from "antd";
 import React, { useContext } from "react";
 import { MdCircle } from "react-icons/md";
@@ -10,88 +11,77 @@ import styles from "../styles/RouteCard.module.css";
 const { Step } = Steps;
 
 const RouteCard = () => {
-  const { routeData } = useContext(RouteContext);
-  return (
-    <Box
-      maxW="sm"
-      borderWidth="1px"
-      //   w="22.5%"
-      mb={20}
-      minW="500px"
-      borderRadius="15px"
-      overflow="hidden"
-      style={{
-        backdropFilter: "blur(10rem)",
-        boxShadow: "6px 6px 20px rgba(122, 122, 122, 0.212)",
-      }}
-      bgGradient="linear(to-br, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.1))"
-    >
-      <div className={styles.routeContainer}>
-        <Steps direction="vertical" current={100}>
-          {routeData.stationsList.map((station, index) => {
-            if (station.lines.length > 1) {
-              return (
-                <Step
-                  title={<h1>{station.station}</h1>}
-                  description={
-                    <>
-                      INTERCHANGE FROM
-                      <Tag
-                        color={
-                          LINE_TO_COLOR[
-                            routeData.stationsList[index - 1].lines[0]
-                          ]
-                        }
-                        style={{ marginLeft: "10px" }}
-                      >
-                        {" "}
-                        {routeData.stationsList[index - 1].lines[0]}{" "}
-                      </Tag>
-                      TO
-                      <Tag
-                        color={
-                          LINE_TO_COLOR[
-                            routeData.stationsList[index + 1].lines[0]
-                          ]
-                        }
-                        style={{ marginLeft: "10px" }}
-                      >
-                        {" "}
-                        {routeData.stationsList[index + 1].lines[0]}
-                      </Tag>
-                    </>
-                  }
-                  icon={
-                    <RiExchangeFill
-                      style={{
-                        color: "Black",
-                        fontSize: "30px",
-                      }}
-                    />
-                  }
-                />
-              );
-            } else {
-              return (
-                <Step
-                  title={<h1>{station.station}</h1>}
-                  description={station.lines[0]}
-                  icon={
-                    <MdCircle
-                      style={{
-                        color: LINE_TO_COLOR[station.lines[0]],
-                        margin: "3px",
-                      }}
-                    />
-                  }
-                />
-              );
-            }
-          })}
-        </Steps>
-      </div>
-    </Box>
-  );
+	const { routeData } = useContext(RouteContext);
+	return (
+		<Box
+			className={styles.routeCard}
+			// maxW='sm'
+			w='22.5%'
+			w={[420, 420, 500]}>
+			<div className={styles.routeContainer}>
+				<Steps direction='vertical' current={100}>
+					{routeData.stationsList.map((station, index) => {
+						if (station.lines.length > 1) {
+							return (
+								<Step
+									title={<h1>{station.station}</h1>}
+									description={
+										<>
+											INTERCHANGE FROM
+											<Tag
+												color={
+													LINE_TO_COLOR[
+														routeData.stationsList[index - 1].lines[0]
+													]
+												}
+												style={{ marginLeft: "10px" }}>
+												{" "}
+												{routeData.stationsList[index - 1].lines[0]}{" "}
+											</Tag>
+											TO
+											<Tag
+												color={
+													LINE_TO_COLOR[
+														routeData.stationsList[index + 1].lines[0]
+													]
+												}
+												style={{ marginLeft: "10px" }}>
+												{" "}
+												{routeData.stationsList[index + 1].lines[0]}
+											</Tag>
+										</>
+									}
+									icon={
+										<RiExchangeFill
+											style={{
+												color: "Black",
+												fontSize: "30px",
+											}}
+										/>
+									}
+								/>
+							);
+						} else {
+							return (
+								<Step
+									title={<h1>{station.station}</h1>}
+									description={station.lines[0]}
+									icon={
+										<MdCircle
+											style={{
+												color: LINE_TO_COLOR[station.lines[0]],
+												margin: "3px",
+											}}
+										/>
+									}
+								/>
+							);
+						}
+					})}
+				</Steps>
+			</div>
+		</Box>
+	);
 };
 
 export default RouteCard;
--- pages/routePage.js
+++ pages/routePage.js
@@ -10,91 +10,97 @@ import StationsSelect from "../components/StationsSelectCard";
 import RouteContext from "../context/routeContext";
 import useGeolocation from "../hooks/useGeoLocation";
 import { Row, Col } from "antd";
+import styles from "../styles/routePage.module.css";
 
 const ROUTE_QUERY = gql`
-  query routeQuery($source: Int, $destination: Int) {
-    route(source: $source, destination: $destination) {
-      time
-      stationsList {
-        station
-        lines
-      }
-      fare
-      interchange
-    }
-  }
+	query routeQuery($source: Int, $destination: Int) {
+		route(source: $source, destination: $destination) {
+			time
+			stationsList {
+				station
+				lines
+			}
+			fare
+			interchange
+		}
+	}
 `;
 
 export default function RoutePage() {
-  const location = useGeolocation();
-  const { routeData, setRouteData } = useContext(RouteContext);
-  const [station, setStation] = useState({
-    source: null,
-    destination: null,
-  });
+	const location = useGeolocation();
+	const { routeData, setRouteData } = useContext(RouteContext);
+	const [station, setStation] = useState({
+		source: null,
+		destination: null,
+	});
 
-  const [runDijkstra, { loading, error, data }] = useLazyQuery(ROUTE_QUERY, {
-    onCompleted: (data) => {
-      const { fare, stationsList, time, interchange } = data.route;
-      setRouteData((prevState) => ({
-        ...prevState,
-        fare,
-        stationsList,
-        time,
-        interchange,
-        loading: !prevState.loading,
-      }));
-      Router.push({
-        pathname: "/routePage",
-        query: { src: station.source, des: station.destination },
-      });
-    },
-  });
+	const [runDijkstra, { loading, error, data }] = useLazyQuery(ROUTE_QUERY, {
+		onCompleted: (data) => {
+			const { fare, stationsList, time, interchange } = data.route;
+			setRouteData((prevState) => ({
+				...prevState,
+				fare,
+				stationsList,
+				time,
+				interchange,
+				loading: !prevState.loading,
+			}));
+			Router.push({
+				pathname: "/routePage",
+				query: { src: station.source, des: station.destination },
+			});
+		},
+	});
 
-  const onClose = () => {
-    setModalOpen(false);
-  };
+	const onClose = () => {
+		setModalOpen(false);
+	};
 
-  const findShortestPath = (source, destination) => {
-    runDijkstra({ variables: { source, destination } });
-    setStation({
-      source,
-      destination,
-    });
-  };
+	const findShortestPath = (source, destination) => {
+		runDijkstra({ variables: { source, destination } });
+		setStation({
+			source,
+			destination,
+		});
+	};
 
-  return (
-    <div>
-      <Navbar />
-      <div
-        className="HeroLayout"
-        style={{ marginLeft: "auto", marginRight: "auto", maxWidth: "1000px" }}
-      >
-        <Row>
-          <Col style={{ marginRight: "20px" }}>
-            <Row>
-              <StationsSelect
-                findShortestPath={findShortestPath}
-                isLoading={routeData.loading}
-                latitude={location.coordinates.lat}
-                longitude={location.coordinates.lng}
-              />
-            </Row>
-            {/* <Row style={{ marginTop: "10px" }}>
+	return (
+		<div>
+			<Navbar />
+			<div
+				className={styles.HeroLayout}
+				// style={{
+				// 	marginTop: "20px",
+				// 	marginLeft: "auto",
+				// 	marginRight: "auto",
+				// 	maxWidth: "1000px",
+				// }}
+			>
+				<Row>
+					<Col style={{ marginRight: "20px" }}>
+						<Row>
+							<StationsSelect
+								findShortestPath={findShortestPath}
+								isLoading={routeData.loading}
+								latitude={location.coordinates.lat}
+								longitude={location.coordinates.lng}
+							/>
+						</Row>
+						{/* <Row style={{ marginTop: "10px" }}>
               <NearestStationCard
                 latitude={location.coordinates.lat}
                 longitude={location.coordinates.lng}
               />
             </Row> */}
-            <Row style={{ marginTop: "10px" }}>
-              <Fare />
-            </Row>
-          </Col>
-          <Col>
-            <RouteCard />
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
+						<Row style={{ marginTop: "10px" }}>
+							<Fare />
+						</Row>
+					</Col>
+					<Col>
+						<RouteCard />
+					</Col>
+				</Row>
+			</div>
+		</div>
+	);
 }
