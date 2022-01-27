b36ecec96a6669e7d18f26313fc34bd2aa06aba6
Ayush Neekhar <a.neekhar01@gmail.com>
2022-01-27 17:15:01 +0530

    Fixed no location permission prompt

--- hooks/useGeoLocation.js
+++ hooks/useGeoLocation.js
@@ -1,47 +1,47 @@
 import { useState, useEffect } from "react";
 
 const useGeoLocation = () => {
-  const [location, setLocation] = useState({
-    loaded: false,
-    coordinates: { lat: "0.0", lng: "0.0" },
-  });
-
-  const onSuccess = (location) => {
-    setLocation({
-      loaded: true,
-      coordinates: {
-        lat: location.coords.latitude,
-        lng: location.coords.longitude,
-      },
+    const [location, setLocation] = useState({
+        loaded: false,
+        coordinates: { lat: "", lng: "" },
     });
-  };
 
-  const onError = (error) => {
-    setLocation({
-      loaded: true,
-      error: {
-        code: error.code,
-        message: error.message,
-      },
-      coordinates: {
-        lat: 0.0,
-        lng: 0.0,
-      },
-    });
-  };
+    const onSuccess = (location) => {
+        setLocation({
+            loaded: true,
+            coordinates: {
+                lat: location.coords.latitude,
+                lng: location.coords.longitude,
+            },
+        });
+    };
 
-  useEffect(() => {
-    if (!("geolocation" in navigator)) {
-      onError({
-        code: 0,
-        message: "Geolocation not supported",
-      });
-    }
+    const onError = (error) => {
+        setLocation({
+            loaded: true,
+            error: {
+                code: error.code,
+                message: error.message,
+            },
+            coordinates: {
+                lat: 0.0,
+                lng: 0.0,
+            },
+        });
+    };
 
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  }, []);
+    useEffect(() => {
+        if (!("geolocation" in navigator)) {
+            onError({
+                code: 0,
+                message: "Geolocation not supported",
+            });
+        }
 
-  return location;
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }, []);
+
+    return location;
 };
 
 export default useGeoLocation;
